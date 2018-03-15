1712e1279d1a736c6040a39c6b12680d334031ce
elmo-space <lorwel@mailbox.org>
2018-03-15 17:00:31 +0100

    fixed spotify integration
    Spotify updated changed the way windows are handled (class names changed) making the script icompatible with spotify.

--- nowplaying.py
+++ nowplaying.py
@@ -15,58 +15,62 @@ green = "\x039"
 
 
 def check_spotify():
-    try:
-        hwnd = win32gui.FindWindow("SpotifyMainWindow", None)
-        return hwnd
-    except Exception as err:
-        hexchat.prnt("Error: %s" % (err))
-        return None
+    def enum_window_callback(hwnd, winclass):
+        if win32gui.IsWindowVisible(hwnd):
+            if winclass == win32gui.GetClassName(hwnd):
+                windows.append(hwnd)
+    windows = []
+    win32gui.EnumWindows(enum_window_callback, "Chrome_WidgetWin_0")
+    for handle in windows:
+        wintitle = win32gui.GetWindowText(handle)
+        if wintitle != "":
+            return handle
 
 
 def check_winamp():
-    try:
-        hwnd = win32gui.FindWindow("Winamp v1.x", None)
-        return hwnd
-    except Exception as err:
-        hexchat.prnt("Error: %s" % (err))
-        return None
+	try:
+		hwnd = win32gui.FindWindow("Winamp v1.x", None)
+		return hwnd
+	except Exception as err:
+		hexchat.prnt("Error: %s" % (err))
+		return None
 
 
 def now_playing(word, wordeol, userdata):
-    win_title = ""
-    hwnd = ""
+	win_title = ""
+	hwnd = ""
 
-    try:
-        if check_spotify():
-            hwnd = check_spotify()
-        elif check_winamp():
-            hwnd = check_winamp()
-        else:
-            raise Exception("No player found...")
-        try:
-            win_title = win32gui.GetWindowText(hwnd)
-            if win_title.startswith("Spotify") or win_title.startswith("Winamp"):
-                raise Exception("Nothing playing...")
-            else:
-                try:
-                    if "Winamp" in win_title:
-                        no_num = re.sub('[0-9]{1,2}\. ', '', win_title)
-                        song_title = re.sub(' - Winamp', '', no_num)
-                    else:
-                        song_title = win_title
-                    artist,title = song_title.split(" - ", 1)
-                    hexchat.command("say %s♫ %sNow playing:%s %s %s-%s %s %s♫" % (red, blue, yellow, artist, blue, green, title, red))
-                except Exception as err:
-                    hexchat.prnt("Error: %s" % (err))
-        except Exception as err:
-            hexchat.prnt("Error: %s" % (err))
-    except Exception as err:
-        hexchat.prnt("Error: %s" % (err))
+	try:
+		if check_spotify():
+			hwnd = check_spotify()
+		elif check_winamp():
+			hwnd = check_winamp()
+		else:
+			raise Exception("No player found...")
+		try:
+			win_title = win32gui.GetWindowText(hwnd)
+			if win_title.startswith("Spotify") or win_title.startswith("Winamp"):
+				raise Exception("Nothing playing...")
+			else:
+				try:
+					if "Winamp" in win_title:
+						no_num = re.sub('[0-9]{1,2}\. ', '', win_title)
+						song_title = re.sub(' - Winamp', '', no_num)
+					else:
+						song_title = win_title
+					artist,title = song_title.split(" - ", 1)
+					hexchat.command("say %s♫ %sNow playing:%s %s %s-%s %s %s♫" % (red, blue, yellow, artist, blue, green, title, red))
+				except Exception as err:
+					hexchat.prnt("Error: %s" % (err))
+		except Exception as err:
+			hexchat.prnt("Error: %s" % (err))
+	except Exception as err:
+		hexchat.prnt("Error: %s" % (err))
 
 
 def main():
-    hexchat.hook_command("music",now_playing,"")
+	hexchat.hook_command("music",now_playing,"")
 
 
 if __name__ == "__main__":
-    main()
+	main()
